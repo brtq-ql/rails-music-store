3a70b1bccc472b67a1626ce006f2bfeb0cf06ccf
brtq-ql <kiprasdom@gmail.com>
2025-11-28 12:38:33 +0200

    Some products updated, products sorted alphabetically

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
 
   def index
-    @products = Product.all
+    @products = Product.order(:name)
   end
 
   def show
